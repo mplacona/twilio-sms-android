33e17cbed9824a4ff520476b09839201310cfc9d
mplacona <marcos.placona@gmail.com>
2016-05-09 16:02:39 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/placona/twiliosms/MainActivity.java
@@ -0,0 +1,81 @@
+package uk.co.placona.twiliosms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private EditText to;
+    private EditText body;
+    private OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        to = (EditText) findViewById(R.id.txtNumber);
+        body = (EditText) findViewById(R.id.txtMessage);
+        final Button send = (Button) findViewById(R.id.btnSend);
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    post("YOUR_NGROK_URL", new  Callback(){
+
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    to.setText("");
+                                    body.setText("");
+                                    Toast.makeText(getApplicationContext(),"SMS Sent!",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    Call post(String url, Callback callback) throws IOException{
+        RequestBody formBody = new FormBody.Builder()
+                .add("To", to.getText().toString())
+                .add("Body", body.getText().toString())
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+
+        Call response = client.newCall(request);
+        response.enqueue(callback);
+        return response;
+
+    }
+}
